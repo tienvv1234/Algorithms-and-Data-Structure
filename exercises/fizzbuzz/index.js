f770538d9db9ef8a26d793cc3553552081c49ec8
tienvvpowergate <tien.vuvan@powergatesoftware.com>
2019-04-10 13:57:28 +0700

    update

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,18 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    for (let index = 1; index <= n; index++) {
+        if (index % 3 === 0 && index % 5 === 0) {
+            console.log('fizzbuzz');
+        } else if (index % 3 === 0) {
+            console.log('fizz');
+        } else if (index % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(index);
+        }
+    }
+}
 
 module.exports = fizzBuzz;
